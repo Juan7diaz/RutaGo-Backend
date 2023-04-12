e0093c520e056a50c8d6f4b112bd4433f30530aa
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2023-04-12 16:44:59 -0500

    agrega la ruta de usuarios

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,26 +1,33 @@
 import express from "express";
 import cors from "cors";
 
+import userRoutes from '../routes/users.routes'
+
 class Server {
-  port: string | undefined;
-  app: any;
+  port: string | undefined
+  app: any
 
   constructor() {
-    this.app = express();
-    this.middlewares();
     this.port = process.env.port;
+    this.app = express()
+    this.middlewares()
+    this.routes()
   }
 
   middlewares() {
-    this.app.use(express.static("public"));
-    this.app.use(express.json());
-    this.app.use(cors());
+    this.app.use(express.static("public"))
+    this.app.use(express.json())
+    this.app.use(cors())
+  }
+
+  routes(){
+    this.app.use('/api/users', userRoutes)
   }
 
   listen() {
     this.app.listen(this.port, () => {
-      console.log(`Server running on port ${this.port}`);
-    });
+      console.log(`Server running on port ${this.port}`)
+    })
   }
 }
 
