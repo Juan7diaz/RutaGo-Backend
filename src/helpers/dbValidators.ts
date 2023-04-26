c9032aa817b22d03f2cea4dd6b84e4e8c91d05ef
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2023-04-26 17:21:12 -0500

    crea validaciones para no repetir email y que exista id

---
+++ src/helpers/dbValidators.ts
@@ -0,0 +1,32 @@
+import User from '../entities/User.entities'
+import AppDataSource from '../database/config'
+
+export const existingEmail = async(email = '') => {
+
+  const UserRepository = AppDataSource.getRepository(User)
+
+  const existingEmail = await UserRepository.findOneBy({
+    email: email,
+    state: true,
+  })
+
+  if(existingEmail){
+    throw new Error(`El ${email} ya se encuentra en uso`)
+  }
+
+}
+
+export const existingUserById = async( id : number ) => {
+
+  const UserRepository = AppDataSource.getRepository(User)
+
+  const userExists = await UserRepository.findOneBy({
+    id: id,
+    state: true,
+  })
+
+  if(!userExists){
+    throw new Error(`EL usuario con id ${id} no existe`)
+  }
+
+}
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import { check } from 'express-validator'
 
 import { getUsers, getUser, postUser, putUser, deleteUser } from '../controllers/users.controllers'
 import { validateFields } from "../middlewares/validateFields";
+import { existingEmail, existingUserById } from '../helpers/dbValidators'
 
 const router = Router()
 
@@ -15,6 +16,7 @@ router.post('/', [
   check('lastName', 'El apellido es obligatorio').notEmpty(),
   check('email', 'El email es obligatorio').notEmpty(),
   check('email', 'El email no se encuentra en el formato correcto').isEmail(),
+  check('email').custom( (email) => existingEmail(email) ),
   check('password', 'La contraseña es obligatoria').notEmpty(),
   check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 }),
   validateFields
@@ -22,15 +24,18 @@ router.post('/', [
 
 router.put('/:id',[
   check('id', 'El id debe ser un numero').isNumeric(),
+  check("id").custom( (id) => existingUserById(id) ),
   check('firstName', 'Se ha pasado un nombre vacio').optional().notEmpty(),
   check('lastName', 'Se ha pasado un apellido vacio').optional().notEmpty(),
   check('email', 'El email no se encuentra en el formato correcto').optional().isEmail(),
+  check('email').custom( (email) => existingEmail(email) ),
   check('password', 'La contraseña debe tener al menos 6 caracteres').optional().isLength({ min: 6 }),
   validateFields
 ],putUser)
 
 router.delete('/:id',[
   check('id', 'El id debe ser un numero').isNumeric(),
+  check("id").custom( (id) => existingUserById(id) ),
   validateFields
 ], deleteUser)
 
