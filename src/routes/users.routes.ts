e3d1e390e924c37e268e7ccb1c841a92b7c5ec8d
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2023-04-26 16:01:56 -0500

    crea validaciones al crear un usuario

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import { check } from 'express-validator'
+
 import { getUsers, getUser, postUser } from '../controllers/users.controllers'
+import { validateFields } from "../middlewares/validateFields";
 
 const router = Router()
 
 router.get('/', getUsers)
 router.get('/:id', getUser)
-router.post('/', postUser)
+router.post('/', [
+  check('firstName', 'El nombre es obligatorio').notEmpty(),
+  check('lastName', 'El apellido es obligatorio').notEmpty(),
+  check('email', 'El email es obligatorio').notEmpty(),
+  check('email', 'El email no se encuentra en el formato correcto').isEmail(),
+  check('password', 'La contraseña es obligatoria').notEmpty(),
+  check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 }),
+  validateFields
+], postUser)
 
 export default router;
