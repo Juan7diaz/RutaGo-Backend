084f0d9880c5a7c82af5557628124b3667d28329
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2023-04-26 16:49:35 -0500

    crea la validaciones al actualizar un usuario

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -17,6 +17,13 @@ router.post('/', [
   check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 }),
   validateFields
 ], postUser)
-router.put('/:id', putUser)
+router.put('/:id',[
+  check('id', 'El id debe ser un numero').isNumeric(),
+  check('firstName', 'Se ha pasado un nombre vacio').optional().notEmpty(),
+  check('lastName', 'Se ha pasado un apellido vacio').optional().notEmpty(),
+  check('email', 'El email no se encuentra en el formato correcto').optional().isEmail(),
+  check('password', 'La contraseña debe tener al menos 6 caracteres').optional().isLength({ min: 6 }),
+  validateFields
+],putUser)
 
 export default router;
